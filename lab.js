7c83d9a7f10594fe0c9ce6c67c122d95e81f8a36
fidaasaed <fidaa.saed97@gmail.com>
2021-03-04 16:52:02 +0200

    finshed

---
+++ lab.js
@@ -0,0 +1,12 @@
+var a =prompt("you favort color");
+console.log("test")
+if (a == "blue")
+{
+    alert("you enterd color is blue")
+}
+else
+{
+    alert("thats good color");
+}
+document.write("you are winner");
+
