a81c5c05e418ed515866013617822a06ff933c23
fidaasaed <fidaa.saed97@gmail.com>
2021-03-07 14:41:40 +0200

    finshed

--- lab.js
+++ lab.js
@@ -1,12 +1,25 @@
-var a =prompt("you favort color");
-console.log("test")
-if (a == "blue")
-{
-    alert("you enterd color is blue")
+// 
+var order =prompt("what do you want flowers or bags");
+while (order !="flowers" && order !="bags")
+{ 
+    order =prompt("what do you want flowers or bags?");
+}
+
+var itemorder ='';
+if (order== "flowers")
+{ 
+    itemorder = '<img src="https://thumbs.dreamstime.com/z/purple-one-tulip-flower-gift-bouquet-neutral-ivory-background-spring-season-flowers-110974216.jpg" />';            
 }
 else
 {
-    alert("thats good color");
+    itemorder = '<img src="https://www.cardfactory.co.uk/dw/image/v2/BCXP_PRD/on/demandware.static/-/Sites-cardfactory-UK-master/default/dw590af06a/images/26532_PRIMARY.jpg?sw=656&sh=656"/>';
 }
-document.write("you are winner");
 
+var numof = prompt(" how many do you want ?");
+var result = '';
+for(var i=0 ; i < numof ; i++ )
+{
+    result = result + itemorder ;
+
+}
+document.write(result);
